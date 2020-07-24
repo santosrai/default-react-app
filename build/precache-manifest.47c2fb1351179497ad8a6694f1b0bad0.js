8dab76420dbbb451209119e52675649bade37114
Santosh Rai <santoshrai@Santoshs-MacBook-Pro.local>
2020-07-24 23:45:41 +0900

    update

---
+++ build/precache-manifest.47c2fb1351179497ad8a6694f1b0bad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664c6a69d63426f65f5a523c0722f790",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10f051804ab19b1f9a02",
+    "url": "/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "c2933edb92406c503cbf",
+    "url": "/static/js/2.9d95fa25.chunk.js"
+  },
+  {
+    "revision": "10f051804ab19b1f9a02",
+    "url": "/static/js/main.246ed18b.chunk.js"
+  },
+  {
+    "revision": "46d27b7d1b66b3825ba8",
+    "url": "/static/js/runtime~main.dfef7c44.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
