70db5df761ec18d2663b2e3a09745a87b7d4a8d3
Santosh Rai <santoshrai@Santoshs-MacBook-Pro.local>
2020-07-25 20:15:01 +0900

    Updates

---
+++ precache-manifest.2c15496e1402f959ff23905338cf6626.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd1cc344052ea1c2c9240cc16173a83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0da2a3bb3c926790c38",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ffc41cddd49356bd73b",
+    "url": "/static/js/2.957b1b4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.957b1b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0da2a3bb3c926790c38",
+    "url": "/static/js/main.db23d5ec.chunk.js"
+  },
+  {
+    "revision": "917c9dd9484e281eb837",
+    "url": "/static/js/runtime-main.8482fd70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e142e7d0a53d474b3882c16ef0be05a9.js"
+  "/precache-manifest.2c15496e1402f959ff23905338cf6626.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
